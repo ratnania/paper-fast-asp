de4eedc1a24ac2215d391344a69e757c5ce24fbc
imadki <kissami.imad@gmail.com>
2022-04-21 12:54:57 +0000

    update multi_index

--- algorithms/kroneker.py
+++ algorithms/kroneker.py
@@ -1,6 +1,6 @@
   
-def multi_index():
-    pass
+# def multi_index():
+#     pass
 
 def kroneker_lower(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
                    A2_data: 'float[:]', A2_ind: 'int32[:]', A2_ptr: 'int32[:]',
@@ -21,9 +21,9 @@ def kroneker_lower(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
     for i1 in range(1, n1):
         for i2 in range(1, n2):
             for i3 in range(1, n3):
-                i = multi_index(i1,i2,i3)
-                yi = 0
-                ad = 1
+                i = i3 + (i2 + i1 * n2) * n3#multi_index(i1,i2,i3)
+                yi = 0.
+                ad = 1.
                 for k1 in range(A1_ptr[i1], A1_ptr[i1+1]):
                     j1 = A1_ind[k1]
                     a1 = A1_data[k1]
@@ -33,13 +33,13 @@ def kroneker_lower(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
                         for k3 in range(A3_ptr[i3], A3_ptr[i3+1]):
                             j3 = A3_ind[k3]
                             a3 = A3_data[k3]
-                            j =  multi_index(j1,j2,j3)
+                            j =  j3 + (j2 + j1 * n2) * n3#multi_index(j1,j2,j3)
                             if i < j:
                                 yi = yi + a1*a2*a3*x[j]
                             else:
                                 ad = a1*a2*a3
-                zi = 0
-                bd = 0
+                zi = 0.
+                bd = 0.
                 for k1 in range(B1_ptr[i1], B1_ptr[i1+1]):
                     j1 = B1_ind[k1]
                     a1 = B1_data[k1]
@@ -49,13 +49,13 @@ def kroneker_lower(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
                         for k3 in range(B3_ptr[i3], B3_ptr[i3+1]):
                             j3 = B3_ind[k3]
                             a3 = B3_data[k3]
-                            j =  multi_index(j1,j2,j3)
+                            j =  j3 + (j2 + j1 * n2) * n3#multi_index(j1,j2,j3)
                             if i < j:
                                 zi = zi + a1*a2*a3*x[j]
                             else:
                                 bd = a1*a2*a3
-                wi = 0
-                cd = 0
+                wi = 0.
+                cd = 0.
                 for k1 in range(C1_ptr[i1], C1_ptr[i1+1]):
                     j1 = C1_ind[k1]
                     a1 = C1_data[k1]
@@ -65,7 +65,7 @@ def kroneker_lower(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
                         for k3 in range(C3_ptr[i3], C3_ptr[i3+1]):
                             j3 = C3_ind[k3]
                             a3 = C3_data[k3]
-                            j =  multi_index(j1,j2,j3)
+                            j =  j3 + (j2 + j1 * n2) * n3#multi_index(j1,j2,j3)
                             if i < j:
                                 wi = zi + a1*a2*a3*x[j]
                             else:
@@ -93,9 +93,9 @@ def kroneker_upper(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
     for i1 in range(1, n1):
         for i2 in range(1, n2):
             for i3 in range(1, n3):
-                i = multi_index(i1,i2,i3)
-                yi = 0
-                ad = 1
+                i = i3 + (i2 + i1 * n2) * n3#multi_index(i1,i2,i3)
+                yi = 0.
+                ad = 1.
                 for k1 in range(A1_ptr[i1], A1_ptr[i1+1]):
                     j1 = A1_ind[k1]
                     a1 = A1_data[k1]
@@ -105,13 +105,13 @@ def kroneker_upper(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
                         for k3 in range(A3_ptr[i3], A3_ptr[i3+1]):
                             j3 = A3_ind[k3]
                             a3 = A3_data[k3]
-                            j =  multi_index(j1,j2,j3)
+                            j =  j3 + (j2 + j1 * n2) * n3#multi_index(j1,j2,j3)
                             if i >= j:
                                 yi = yi + a1*a2*a3*x[j]
                             else:
                                 ad = a1*a2*a3
-                zi = 0
-                bd = 0
+                zi = 0.
+                bd = 0.
                 for k1 in range(B1_ptr[i1], B1_ptr[i1+1]):
                     j1 = B1_ind[k1]
                     a1 = B1_data[k1]
@@ -121,13 +121,13 @@ def kroneker_upper(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
                         for k3 in range(B3_ptr[i3], B3_ptr[i3+1]):
                             j3 = B3_ind[k3]
                             a3 = B3_data[k3]
-                            j =  multi_index(j1,j2,j3)
+                            j =  j3 + (j2 + j1 * n2) * n3#multi_index(j1,j2,j3)
                             if i >= j:
                                 zi = zi + a1*a2*a3*x[j]
                             else:
                                 bd = a1*a2*a3
-                wi = 0
-                cd = 0
+                wi = 0.
+                cd = 0.
                 for k1 in range(C1_ptr[i1], C1_ptr[i1+1]):
                     j1 = C1_ind[k1]
                     a1 = C1_data[k1]
@@ -137,7 +137,7 @@ def kroneker_upper(A1_data: 'float[:]', A1_ind: 'int32[:]', A1_ptr: 'int32[:]',
                         for k3 in range(C3_ptr[i3], C3_ptr[i3+1]):
                             j3 = C3_ind[k3]
                             a3 = C3_data[k3]
-                            j =  multi_index(j1,j2,j3)
+                            j =  j3 + (j2 + j1 * n2) * n3#multi_index(j1,j2,j3)
                             if i >= j:
                                 wi = zi + a1*a2*a3*x[j]
                             else:
